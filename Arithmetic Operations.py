98e9829c2a4167296d5ad5b9997969b953abf10b
SyedMahdi1 <131530472+SyedMahdi1@users.noreply.github.com>
2024-04-03 05:08:23 -0700

    Enjoy The Codes
    New Lesson. YEAH!!.   :) 😎😎😎😎😎😎😎😎😎

---
+++ Arithmetic Operations.py
@@ -0,0 +1,22 @@
+# For integers
+a = 2
+# a = a + 2 # Long method
+# a += 2 # Short Method
+print(a)
+
+# For variables
+b = ("Mahdi", "Raza", "Ali", "LoL")
+# b = b + ("Lamao",)# Long
+# b += "Moye Moye"# Short
+print(b)
+# For removing
+c = 2
+c = c - 1
+c -= 0.5
+print(c)
+
+# Variable
+
+d = "MahdiRaza"
+d = d[:-4]
+print(d)
